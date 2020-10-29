6c2d275fef3bb11cf648742663cd3d778d87d363
Yu Hou <yuhou2@illinois.edu>
2020-10-29 18:02:39 -0500

    update serializers

--- backend/all4cats/serializers.py
+++ backend/all4cats/serializers.py
@@ -1,12 +1,36 @@
 from rest_framework import serializers 
-# from all4cats.models import All4Cats
+from all4cats.models import Region, Price, University, House
  
  
-# class All4CatsSerializer(serializers.ModelSerializer):
- 
-#     class Meta:
-#         model = All4Cats
-#         fields = ('id',
-#                   'title',
-#                   'description',
-#                   'published')
+class regionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('zipcode',
+                  'state',
+                  'city')
+
+
+class priceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Price
+        fields = ('date',
+                  'value',
+                  'zipcode')
+
+
+class univeristySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = University
+        fields = ('university_name',
+                  'zipcode')
+
+
+class HouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = House
+        fields = ('house_id',
+                  'price',
+                  'company',
+                  'address',
+                  'number_of_rooms',
+                  'floor_plan')
