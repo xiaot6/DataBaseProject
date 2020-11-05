7a2a58dea0dd930cd62ce5bac955fecd44fbfe34
Zihan Xu <zihanxu3@illinois.edu>
2020-11-05 17:26:06 -0600

    update database user

--- backend/all4cats_api/settings.py
+++ backend/all4cats_api/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testdb',
         'USER': 'root',
-        'PASSWORD': '14704718Xu',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
