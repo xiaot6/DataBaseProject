02ce66b0be88ccc91505fe034f651fd3486e87da
Xiao Tan <xiaot6@illinois.edu>
2020-11-29 21:48:01 -0600

    push pictures

---
+++ all4cats/src/components/States/CA.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class CA extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <div>
+            <h2>
+                This is CA.
+            </h2>
+        </div>
+    )
+  }
+}
