1b8f5d13274c48f37870d99bf4a38d02cc852929
Zihan Xu <zihanxu3@illinois.edu>
2020-12-01 01:05:40 -0600

    user base defined

--- all4cats/src/components/ProfilePage.js
+++ all4cats/src/components/ProfilePage.js
@@ -1,14 +1,24 @@
 import React, { useContext } from "react";
 import { UserContext } from "../providers/UserProvider";
-import {auth} from "../firebase";
+import { auth, addFavoriteLayoutAndType } from "../firebase";
 import {makeStyles} from '@material-ui/core/styles';
-import { Button, Typography } from "@material-ui/core";
+import { Button, Typography, Card, TextField, InputAdornment, OutlinedInput } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+      paddingTop: "100px",
       display: 'flex',
-      flexDirection: "column",
-      justifyContent: "center"
+      // flexDirection: "column",
+      justifyContent: "center",
+  },
+  card: {
+    display: 'flex',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   },
 }));
 
@@ -16,19 +26,95 @@ const ProfilePage = () => {
   const user = useContext(UserContext);
   const {displayName, email} = user;
   const classes = useStyles();
+  const [bedrooms, setBedrooms] = React.useState('');
+  const [bathrooms, setBathrooms] = React.useState('');
+
+  const [lowerPrice, setLowerPrice] = React.useState('');
+  const [upperPrice, setUpperPrice] = React.useState('');
+
   console.log(displayName);
   console.log(email);
+
+
+  const onChangeSaveBedrooms = (event) => {
+    setBedrooms(event.target.value);
+  };
+
+  const onChangeSaveBathrooms = (event) => {
+    setBathrooms(event.target.value);
+  };
+
+  const onChangeSaveLowerPrice = (event) => {
+    setLowerPrice(event.target.value);
+  };
+
+  const onChangeSaveUpperPrice = (event) => {
+    setUpperPrice(event.target.value);
+  };
+
+  function onClickSavePreferences(user, bed, bath, lower, upper) {
+    console.log(bed);
+    console.log(bath);
+    addFavoriteLayoutAndType(user, bed, bath, lower, upper);
+  };
+
   return (
     <div className={classes.root}>
-      <Typography>
-        {displayName}
-      </Typography>
-      <Typography>
-        {email}
-      </Typography>
-      <Button onClick = {() => {auth.signOut()}}>
-        Sign Out
-      </Button>
+      <div style={{width:"320px"}}>
+        <Card variant="outlined">
+            <div style={{display: 'flex', flexDirection: "column", justifyContent:"flex-start"}}>
+            <h2>
+              My Profile
+            </h2>
+            <Typography>
+              Display Name: {displayName}
+            </Typography>
+            <Typography>
+              Email: {email}
+            </Typography>
+            </div>
+            <Button variant="contained" color="secondary" style={{marginTop:"1rem", marginBottom:"0.5rem"}} onClick = {() => {auth.signOut()}}>
+              Sign Out
+            </Button>
+        </Card>
+        <Card style={{marginTop: "2rem"}} variant="outlined">
+            <h2>
+              My Preference
+            </h2>
+            <Typography>
+              Favorite Layout: 
+            </Typography>
+            <div style={{margin: "2px 15px", display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center'}}>
+              <TextField margin='dense' size="small" style={{width: "40px"}} value={bedrooms} onChange={onChangeSaveBedrooms} variant="outlined">
+              </TextField>
+              <Typography>
+                Bedrooms
+              </Typography>
+              <TextField margin='dense' size="small" style={{width: "40px"}} value={bathrooms} onChange={onChangeSaveBathrooms} variant="outlined"></TextField>
+              <Typography>
+                Bathrooms
+              </Typography>
+            </div>
+
+            <Typography style={{marginTop: "20px", marginBottom: "10px"}}>
+              Favorite Price Range: 
+            </Typography>
+            <div style={{margin: "2px 15px", display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center'}}>
+              <Typography>
+                Lower
+              </Typography>
+              <OutlinedInput startAdornment={<InputAdornment position="start">$</InputAdornment>} margin='dense' size="small" style={{width: "90px"}} value={lowerPrice} onChange={onChangeSaveLowerPrice} variant="outlined"></OutlinedInput>
+              <Typography>
+                Upper
+              </Typography>
+              <OutlinedInput startAdornment={<InputAdornment position="start">$</InputAdornment>} margin='dense' size="small" style={{width: "90px"}} value={upperPrice} onChange={onChangeSaveUpperPrice} variant="outlined"></OutlinedInput>
+            </div>
+
+            <Button variant="contained" color="primary" style={{margin: "15px 0px"}} onClick={() => onClickSavePreferences(user, bedrooms, bathrooms, lowerPrice, upperPrice)}>
+              Save Preferences
+            </Button>
+        </Card>
+      </div>
     </div>
   ) 
 };
