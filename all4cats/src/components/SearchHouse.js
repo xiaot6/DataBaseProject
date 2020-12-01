c308fabf3c6e0011e30a7546f7936179c3c5893c
Kino <yuxinw5@illinois.edu>
2020-12-01 13:39:21 -0600

    edit search house page

--- all4cats/src/components/SearchHouse.js
+++ all4cats/src/components/SearchHouse.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import HouseDataService from "../services/house.service";
-import {Button, TextField} from '@material-ui/core'
+import {Button, TextField, List} from '@material-ui/core'
 import {auth, addFavoriteHouse} from '../firebase';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { GridList, GridListTile, Grid } from '@material-ui/core'
 
 export default class SearchHouse extends Component {
     constructor(props) {
@@ -88,14 +92,42 @@ export default class SearchHouse extends Component {
                 <Button onClick={this.searchByBed}>
                     Search By Number of Bedrooms
                 </Button>
-                <Card>
-                <CardContent>
-                {this.state.houseArrayJSON &&
-                this.state.houseArrayJSON.map((houseJSON, index) => (
-                    <Typography>Address: {houseJSON.address}, Company: {houseJSON.company}, Floor Plan: {houseJSON.floor_plan}, Number of Rooms: {houseJSON.number_of_rooms}, Price: {houseJSON.price}</Typography>
-                ))}
-                </CardContent>
-                </Card>
+                <div style={{display: 'flex',
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-around',
+                            overflow: 'hidden',
+                            marginTop: "50px"}}>
+                    <GridList cellHeight={330} style={{width: 1000}}>
+                        {this.state.houseArrayJSON &&
+                        this.state.houseArrayJSON.map((houseJSON, index) => (
+                            <GridListTile key={index} style={{display: 'flex', justifyContent: 'center'}}>
+                                <Card style={{width: 400, height: 330}} variant='outlined'>
+                                    <CardMedia style={{height: 0,
+                                                paddingTop: '43.25%'}} 
+                                                image="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"/>
+                                    <CardContent style={{flexGrow: 1}}>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={6}>
+                                                <Typography>Company: {houseJSON.company}</Typography>
+                                                <Typography>Floor Plan: {houseJSON.floor_plan}</Typography>
+                                                <Typography>Number of Rooms: {houseJSON.number_of_rooms}</Typography>
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Typography>Address: {houseJSON.address}</Typography>
+                                                <Typography>Price: {houseJSON.price}</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </CardContent>
+                                    <CardActions disableSpacing>
+                                        <IconButton aria-label="add to favorites">
+                                            <FavoriteIcon />
+                                        </IconButton>
+                                    </CardActions>
+                                </Card>
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                </div>
             </div>
         )
     }
